a21cdaf2ad433747871f5d00bf8ffdd395131ded
Vishal Kumar <vikrantkvishal@gmail.com>
2024-08-08 11:28:02 +0530

    VS Code Commit

--- day1.c
+++ day1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-	printf("Hello Hello Hello)";
+	printf("Hello Hello)";
 	return;
 }
 
--- trial.c
+++ trial.c
@@ -1,4 +1,4 @@
 void main(){
-	printf("Hello there"):
+	printf("Hello there hello"):
 	return;
 }
